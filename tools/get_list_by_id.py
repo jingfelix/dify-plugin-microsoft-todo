1c94e112eb52a3d56730d32d568e95b9f1544015
jingfelix <jingfelix@outlook.com>
2025-07-29 11:14:53 +0800

    feat: support dynamic-select (get_list_by_id)
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- tools/get_list_by_id.py
+++ tools/get_list_by_id.py
@@ -1,10 +1,14 @@
+import logging
 from collections.abc import Generator
 from typing import Any
 
 from dify_plugin import Tool
+from dify_plugin.entities import I18nObject, ParameterOption
 from dify_plugin.entities.tool import ToolInvokeMessage
 
 from src.mstodo import *
+
+logger = logging.getLogger(__name__)
 
 
 class DifyPluginPdmTemplateTool(Tool):
@@ -27,3 +31,32 @@ class DifyPluginPdmTemplateTool(Tool):
                 yield self.create_json_message(res)
             else:
                 yield self.create_text_message(str(res))
+
+    def _fetch_parameter_options(self, parameter: str) -> list[ParameterOption]:
+
+        logger.debug(f"Fetching parameter options for: {parameter}")
+
+        match parameter:
+            case "list_id":
+                token: Token = Token(**json.loads(self.runtime.credentials["token"]))
+                if not token:
+                    raise ValueError("Token is required to invoke this tool.")
+
+                todo_client = ToDoConnection(
+                    client_id="",
+                    client_secret="",
+                    token=token,
+                )
+
+                return [
+                    ParameterOption(
+                        value=task_list.list_id,
+                        label=I18nObject(en_US=task_list.displayName),
+                    )
+                    for task_list in todo_client.get_lists()
+                ]
+            case _:
+                logger.error(f"Unsupported parameter: {parameter}")
+                raise NotImplementedError(
+                    "Unsupported parameter" f"{parameter} for this tool."
+                )
